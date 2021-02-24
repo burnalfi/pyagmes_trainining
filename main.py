3fc95cde6707bacdb2e434dfa6f636b3a1a5aacc
Michael Orlando <michaelorlando20@gmail.com>
2021-02-24 19:36:44 +0700

    Sprite Texturing and Basic Game Mechanics

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sprite import Player
+from sprite import Player, Enemy, Cloud
 
 import pygame
 
@@ -10,8 +10,22 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 1000)
+
 running = True
 player = Player()
+    
+clock = pygame.time.Clock()
+
+enemies = pygame.sprite.Group()
+clouds = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 while running:
     for event in pygame.event.get():
@@ -22,10 +36,31 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
+        elif event.type == ADDCLOUD:
+            new_cloud = Cloud()
+            clouds.add(new_cloud)
+            all_sprites.add(new_cloud)
+
+    screen.fill((135, 206, 250))
+
     pressed_key = pygame.key.get_pressed()
     player.update(pressed_key)
 
-    screen.blit(player.surf , player.rect)
+    enemies.update()
+    clouds.update()
+
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    if pygame.sprite.spritecollideany(player, enemies):
+        player.kill()
+        running = False
     
     pygame.display.flip()
+
+    clock.tick(30)
--- sprite.py
+++ sprite.py
@@ -1,6 +1,7 @@
 import pygame
+import random
 
-from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT
+from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT, RLEACCEL
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -8,19 +9,20 @@ SCREEN_HEIGHT = 600
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((25, 25))
-        self.surf.fill((255, 255, 255))
+        self.surf = pygame.image.load("images/jet_fighter_PNG110.png").convert()
+        self.surf = pygame.transform.scale(self.surf, (70, 70))
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect()
 
     def update(self, pressed_key):
         if pressed_key[K_UP]:
-            self.rect.move_ip(0, -1)
+            self.rect.move_ip(0, -5)
         if pressed_key[K_DOWN]:
-            self.rect.move_ip(0, 1)
+            self.rect.move_ip(0, 5)
         if pressed_key[K_RIGHT]:
-            self.rect.move_ip(1, 0)
+            self.rect.move_ip(5, 0)
         if pressed_key[K_LEFT]:
-            self.rect.move_ip(-1, 0)
+            self.rect.move_ip(-5, 0)
 
         if self.rect.left < 0:
             self.rect.left = 0
@@ -30,3 +32,41 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load("images/pngfind.com-missile-png-455052.png").convert()
+        self.surf = pygame.transform.scale(self.surf, (20, 10))
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+        # self.speed = random.randint(1, 2)
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load("images/37-373579.png").convert()
+        self.surf = pygame.transform.scale(self.surf, (50, 70))
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+
+    def update(self):
+        self.rect.move_ip(-5, 0)
+        if self.rect.right < 0:
+            self.kill()
